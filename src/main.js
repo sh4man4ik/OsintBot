672d8fdca9d286fef73bfc1922eda408f988c00f
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-07-17 20:58:30 +0300

    Bot setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+import { Telegraf } from 'telegraf';
+import dotenv from 'dotenv';
+import { message } from 'telegraf/filters';
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.on(message('text'), async (ctx) => {
+	await ctx.reply(`Hello, World!`);
+});
+
+bot.launch();
