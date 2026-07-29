1ca06496368f48d88732d2f00faee902c46075eb
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-07-29 18:04:09 +0300

    Bug fixes #2

--- src/handlers/menu/api/getData.js
+++ src/handlers/menu/api/getData.js
@@ -8,7 +8,26 @@ export async function getData(ctx, link) {
 	let data;
 
 	try {
-		const response = await fetch(link);
+		const response = await fetch(link, {
+			headers: {
+				'User-Agent':
+					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+				Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+				'Accept-Language': 'en-US,en;q=0.9',
+				'Accept-Encoding': 'gzip, deflate, br',
+				'Cache-Control': 'no-cache',
+				Pragma: 'no-cache',
+				'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+				'Sec-Ch-Ua-Mobile': '?0',
+				'Sec-Ch-Ua-Platform': '"Windows"',
+				'Sec-Fetch-Dest': 'document',
+				'Sec-Fetch-Mode': 'navigate',
+				'Sec-Fetch-Site': 'none',
+				'Sec-Fetch-User': '?1',
+				'Upgrade-Insecure-Requests': '1',
+				Referer: 'https://duckduckgo.com/'
+			}
+		});
 
 		if (!response.ok) {
 			throw new Error(response.status);
